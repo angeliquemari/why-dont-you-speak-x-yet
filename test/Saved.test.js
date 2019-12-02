22468bf85d1530bb76fae5142230fc4686c792ba
Angelique Nehmzow <angelique@nehmzow.com>
2019-12-01 23:11:46 -0800

    Install and configure jest & enzyme, first test for Saved component

---
+++ test/Saved.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Saved from '../client/src/Saved.jsx';
+import { deleteTranslation, updateFilter } from '../client/src/helpers.js';
+
+describe('Saved Component', () => {
+  it('renders', () => {
+    shallow(
+      <Saved
+        translations={[]}
+        deleteTranslation={deleteTranslation}
+        filter={''}
+        updateFilter={updateFilter}
+      />
+    );
+  });
+
+  // option elements = languages in translations + 1
+
+  // if filter === '' # of tr elements is same as # of translations
+
+  // if filter exists, translations shown are only of that language
+});
